c8dcedd9300399d1c0b31b7ca071956a419eb160
OguzhanIrmak <oguzhanirmak01@gmail.com>
2024-07-27 15:05:28 +0300

    INotifyPropertyChanged

--- Models/Urun.cs
+++ Models/Urun.cs
@@ -1,15 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfGridComboOrnek.Models
 {
-    public class Urun
+    public class Urun:INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Adi { get; set; }
-        public int KategoriId { get; set; }
+        int id;
+        string adi;
+        int kategoriId;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Adi
+        {
+            get
+            {
+                return adi;
+            }
+            set
+            {
+                adi = value;
+                OnPropertyChanged("Adi");
+            }
+        }
+        public int KategoriId
+        {
+            get
+            {
+                return kategoriId;
+            }
+            set
+            {
+                kategoriId = value;
+                OnPropertyChanged("KategoriId");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
